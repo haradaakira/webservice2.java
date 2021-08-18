992bb9d66f28f9d68f06a2112a213b0eedc037ee
Akira Harada <i.jun@outlook.com>
2021-08-17 21:28:32 -0300

    Login and password recover page css done.

--- src/main/java/com/unesa/integrador/controller/RecoverPasswordController.java
+++ src/main/java/com/unesa/integrador/controller/RecoverPasswordController.java
@@ -2,6 +2,8 @@ package com.unesa.integrador.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecoverPasswordController {
@@ -10,6 +12,12 @@ public class RecoverPasswordController {
     public static String recoverPasswordPage() {
 
         return "recoverpage";
+    }
+
+    @RequestMapping("/recovercode")
+    public static String recoverCodePage() {
+
+        return "recovervalidationpage";
     }
 
 }
