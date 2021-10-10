156a5a106511993a3a8260c2047b3efddf766746
Akira Harada <i.jun@outlook.com>
2021-10-10 16:48:04 -0300

    Register done, saving into DB.

--- src/main/java/com/unesa/integrador/controller/RegisterUserController.java
+++ src/main/java/com/unesa/integrador/controller/RegisterUserController.java
@@ -1,15 +1,33 @@
 package com.unesa.integrador.controller;
 
+import com.unesa.integrador.model.Financeira;
+import com.unesa.integrador.repository.FinanceiraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterUserController {
 
-    @GetMapping("/register")
-    public static String registerPage() {
+    @Autowired
+    FinanceiraRepository financeiraRepository;
 
+    @GetMapping("/register")
+    public String registerPage(ModelMap model) {
+        model.addAttribute("registerobject", new Financeira());
         return "registerpage";
+    }
+
+    @PostMapping("/register")
+    public String submitregister(ModelMap model, @ModelAttribute("registerobject")Financeira financeira, BindingResult result) {
+        financeira.setId(financeiraRepository.getMaxId() + 1);
+        System.out.println(financeira.toString());
+        financeiraRepository.save(financeira);
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/unesa/integrador/repository/FinanceiraRepository.java
+++ src/main/java/com/unesa/integrador/repository/FinanceiraRepository.java
@@ -2,6 +2,7 @@ package com.unesa.integrador.repository;
 
 import com.unesa.integrador.model.Financeira;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface FinanceiraRepository extends JpaRepository<Financeira, String> {
     List<Financeira> findAll();
+
+    @Query(value = "SELECT coalesce(max(id),0) FROM Financeira")
+    public int getMaxId();
 }
