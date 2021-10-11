826e7e592597b1d93a8ff936cb33a7449480861c
Akira Harada <i.jun@outlook.com>
2021-10-11 18:35:13 -0300

    Login methods at Controller and HTML done. Validation ok.

--- src/main/java/com/unesa/integrador/controller/LoginController.java
+++ src/main/java/com/unesa/integrador/controller/LoginController.java
@@ -1,15 +1,23 @@
 package com.unesa.integrador.controller;
 
+import com.unesa.integrador.model.Financeira;
 import com.unesa.integrador.repository.EnderecoRepository;
+import com.unesa.integrador.repository.FinanceiraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Objects;
 
 @Controller
 public class LoginController {
 
     @Autowired
-    static EnderecoRepository a;
+    FinanceiraRepository financeiraRepository;
 
     @GetMapping("/")
     public static String redirectLoginPage() {
@@ -17,9 +25,25 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public static String loginPage() {
-//        System.out.println(a.findById("1").get().getBairro());
+    public static String loginPage(ModelMap model) {
+        model.addAttribute("loginobject", new Financeira());
         return "loginpage";
     }
 
+    @PostMapping("/login")
+    public String loginValidator(ModelMap model, @ModelAttribute("loginobject")Financeira financeira, BindingResult result) {
+        Financeira fromdb = financeiraRepository.findByCNPJ(financeira.getCnpj());
+        if (financeira.getSenha() == null || Objects.equals(financeira.getSenha(), "")) {
+            return "redirect:/login";
+        }
+        try {
+            if (Objects.equals(fromdb.getSenha(), financeira.getSenha())) {
+                return "redirect:/menu";
+            }
+        } catch (Exception err) {
+            return "redirect:/login";
+        }
+
+        return "redirect:/login";
+    }
 }
--- src/main/java/com/unesa/integrador/repository/FinanceiraRepository.java
+++ src/main/java/com/unesa/integrador/repository/FinanceiraRepository.java
@@ -3,6 +3,7 @@ package com.unesa.integrador.repository;
 import com.unesa.integrador.model.Financeira;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public interface FinanceiraRepository extends JpaRepository<Financeira, String> {
     List<Financeira> findAll();
+
+    @Query(value = "SELECT * FROM FINANCEIRA WHERE CNPJ = :cnpj", nativeQuery = true)
+    public Financeira findByCNPJ(@Param("cnpj") String cnpj);
 
     @Query(value = "SELECT coalesce(max(id),0) FROM Financeira")
     public int getMaxId();
