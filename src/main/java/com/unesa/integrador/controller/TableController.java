baadf301365a76a6583bffba522c96d7744f230f
Akira Harada <i.jun@outlook.com>
2021-10-24 22:19:07 -0300

    Table view done, menu minor fixes with return button.

--- src/main/java/com/unesa/integrador/controller/TableController.java
+++ src/main/java/com/unesa/integrador/controller/TableController.java
@@ -1,13 +1,24 @@
 package com.unesa.integrador.controller;
 
+import com.unesa.integrador.repository.PessoaFisicaRepository;
+import com.unesa.integrador.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TableController {
 
+    @Autowired
+    PessoaFisicaRepository pessoaFisicaRepository;
+
+    @Autowired
+    SessionRepository sessionRepository;
+
     @GetMapping("/loantable")
-    public static String showCustomerTable(){
+    public String showCustomerTable(ModelMap model){
+        model.put("customer", pessoaFisicaRepository.findBySessionOwner(sessionRepository.getDbSessionId()));
         return "customertable";
     }
 }
--- src/main/java/com/unesa/integrador/repository/PessoaFisicaRepository.java
+++ src/main/java/com/unesa/integrador/repository/PessoaFisicaRepository.java
@@ -1,8 +1,10 @@
 package com.unesa.integrador.repository;
 
+import com.unesa.integrador.model.Financeira;
 import com.unesa.integrador.model.PessoaFisica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,8 @@ public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Stri
 
     @Query(value = "SELECT coalesce(max(id),0) FROM PESSOAFISICA", nativeQuery = true)
     public int getMaxId();
+
+    @Query(value = "SELECT * FROM PESSOAFISICA WHERE SESSIONOWNER = :sessionowner", nativeQuery = true)
+    public List<PessoaFisica> findBySessionOwner(@Param("sessionowner") String sessionowner);
+
 }
