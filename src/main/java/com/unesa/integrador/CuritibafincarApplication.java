4e6752607fbac14be37815fad64eb37a88e2f580
Akira Harada <i.jun@outlook.com>
2021-11-07 14:23:31 -0300

    Deploy done, application launch test.

--- src/main/java/com/unesa/integrador/CuritibafincarApplication.java
+++ src/main/java/com/unesa/integrador/CuritibafincarApplication.java
@@ -2,12 +2,19 @@ package com.unesa.integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CuritibafincarApplication {
+public class CuritibafincarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CuritibafincarApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(CuritibafincarApplication.class);
 	}
 
 }
